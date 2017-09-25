53e24533969c4ce7a2bedbf6259e648cb24420c5
Michael Lustenberger <mic@inofix.ch>
2017-09-25 23:32:22 +0200

    Data Reporter
    Add logger methods.

--- libardurep/datareporter.py
+++ libardurep/datareporter.py
@@ -17,11 +17,73 @@ class DataReporter(object):
     to a given URL on request.
     """
 
-    def __init__(self, store):
+    def __init__(self, store, url="", credentials={}, do_verify_certificate=True):
         """
         Initialize the reporter.
             store store         the data store
         """
         self.store = store
+        # for commodity, either register url etc. or choose every time
+        self.url = url
+        self.credentials = credentials
+        self.do_verify_certificate = do_verify_certificate
 
+    def log(self, url=None, credentials=None, do_verify_certificate=None):
+        """
+        Wrapper for the other log methods, decide which one based on the
+        URL parameter.
+        """
+        if url is None:
+            url = self.url
+        if re.match("file://", url):
+            log_file(url)
+        elif re.match("https://", url) or re.match("http://", url):
+            log_post(url, credentials, do_verify_certificate)
+        else:
+            log_stdout()
+
+    def log_stdout(self):
+        """
+        Write to standard output
+        """
+        print self.store.get_text()
+
+    def log_file(self, url=None):
+        """
+        Write to a local log file
+        """
+        if url is None:
+            url = self.url
+        f = re.sub("file://", "", url)
+        try:
+            with open(f, "w") as of:
+                of.write(str(self.data))
+        except IOError as e:
+            print e
+            print "Could not write the content to the file.."
+
+    def log_post(self, url=None, credentials=None, do_verify_certificate=None):
+        """
+        Write to a remote host via HTTP POST
+        """
+        if url is None:
+            url = self.url
+        if credentials is None:
+            credentials = self.credentials
+        if do_verify_certificate is None:
+            do_verify_certificate = self.do_verify_certificate
+        if credentials and credentials.has_key("base64"):
+            headers = {"Content-Type": "application/json", 'Authorization': 'Basic %s' % credentials["base64"]}
+        else:
+            headers = {"Content-Type": "application/json"}
+        try:
+            request = requests.post(url, headers=headers, data=self.data, verify=do_verify_certificate)
+        except httplib.IncompleteRead as e:
+            request = e.partial
+
+    def log_ssh(self):
+        """
+        Write to a remote file via ssh
+        """
+        pass
 
