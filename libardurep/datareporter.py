f5b184bfd90048b732fdf763e0e491f2f7824a77
Michael Lustenberger <mic@inofix.ch>
2017-09-25 23:56:30 +0200

    Data Report
    Add credentials and fix method call for log.

--- libardurep/datareporter.py
+++ libardurep/datareporter.py
@@ -8,8 +8,9 @@ COPYRIGHT:    (C) 2017 by Michael Lustenberger and INOFIX GmbH
               License (v3).
 """
 
-import datetime
-import json
+#import datetime
+#import json
+import re
 
 class DataReporter(object):
     """
@@ -36,11 +37,11 @@ class DataReporter(object):
         if url is None:
             url = self.url
         if re.match("file://", url):
-            log_file(url)
+            self.log_file(url)
         elif re.match("https://", url) or re.match("http://", url):
-            log_post(url, credentials, do_verify_certificate)
+            self.log_post(url, credentials, do_verify_certificate)
         else:
-            log_stdout()
+            self.log_stdout()
 
     def log_stdout(self):
         """
@@ -73,11 +74,13 @@ class DataReporter(object):
         if do_verify_certificate is None:
             do_verify_certificate = self.do_verify_certificate
         if credentials and credentials.has_key("base64"):
-            headers = {"Content-Type": "application/json", 'Authorization': 'Basic %s' % credentials["base64"]}
+            headers = {"Content-Type": "application/json", \
+                        'Authorization': 'Basic %s' % credentials["base64"]}
         else:
             headers = {"Content-Type": "application/json"}
         try:
-            request = requests.post(url, headers=headers, data=self.data, verify=do_verify_certificate)
+            request = requests.post(url, headers=headers, data=self.data, \
+                                                 verify=do_verify_certificate)
         except httplib.IncompleteRead as e:
             request = e.partial
 
@@ -86,4 +89,54 @@ class DataReporter(object):
         Write to a remote file via ssh
         """
         pass
+
+    def register_credentials(self, credential=None, user=None, user_file=None, password=None, password_file=None):
+        """
+        Helper method to store username and password
+        """
+        # lets store all kind of credential data into this dict
+        if credentials is not None:
+            self.credentials = credentials
+        else:
+            self.credentials = {}
+            # set the user from CLI or file
+            if user:
+                self.credentials["user"] = user
+            elif user_file:
+                with open(user_file, "r") as of:
+                    # what would the file entry look like?
+                    pattern = re.compile("^user: ")
+                    for l in of:
+                        if re.match(pattern, l):
+                            # strip away the newline
+                            l = l[0:-1]
+                            self.credentials["user"] = re.sub(pattern, "", l)
+            # remove any surrounding quotes
+            if self.credentials["user"][0:1] == '"' and \
+                                    self.credentials["user"][-1:] == '"':
+                self.credentials["user"] = self.credentials["user"][1:-1]
+            # set the password from CLI or file
+            if password:
+                self.credentials["password"] = password
+            elif password_file:
+                with open(password_file, "r") as of:
+                    # what would the file entry look like?
+                    pattern = re.compile("^password: ")
+                    for l in of:
+                        if re.match(pattern, l):
+                            # strip away the newline
+                            l = l[0:-1]
+                            self.credentials["password"] = \
+                                                    re.sub(pattern, "", l)
+            # remove any surrounding quotes
+            if self.credentials["password"][0:1] == '"' and \
+                                    self.credentials["password"][-1:] == '"':
+                self.credentials["password"] = \
+                                        self.credentials["password"][1:-1]
+
+            # if both user and password is set,
+            #  1. encode to base 64 for basic auth
+            if self.credentials["user"] and self.credentials["password"]:
+                self.credentials["base64"] = b64encode(self.credentials["user"]\
+                        + ":" + self.credentials["password"]).decode("ascii")
 
