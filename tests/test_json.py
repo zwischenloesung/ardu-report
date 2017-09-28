0f5f476747642227f1fd681d6ac0ba8e80ace8bc
Michael Lustenberger <mic@inofix.ch>
2017-09-28 22:48:19 +0200

    JSON Schema
    Reference the meta schema and add tests.

--- tests/test_json.py
+++ tests/test_json.py
@@ -6,10 +6,15 @@ import os
 
 class TestJSON(unittest.TestCase):
     def setUp(self):
+        with open("./schemas/meta-schema.json", "r") as fh:
+            self.meta_schema = json.loads(fh.read())
         with open("./schemas/default-schema.json", "r") as fh:
             self.schema = json.loads(fh.read())
         with open("./examples/input.json") as fh:
             self.example = json.loads(fh.read())
+
+    def test_schema(self):
+        Validator(self.meta_schema).validate(self.schema)
 
     def test_example(self):
         Validator(self.schema).validate(self.example)
